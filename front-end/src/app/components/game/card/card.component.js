5f711d0fa622a25f78b55d7452b74dd967c76e71
juliaMasset <100589004+juliaMasset@users.noreply.github.com>
2022-04-20 10:56:37 +0200

    changed function to class in html

--- front-end/src/app/components/game/card/card.component.js
+++ front-end/src/app/components/game/card/card.component.js
@@ -4,58 +4,53 @@
 (function () {
   // TODO Step 7 remove this closure
 
-  // TODO Step 3.1 create a class
-  /* class CardComponent constructor */
-  function CardComponent(id) {
-    // is this card flipped ?
-    this._flipped = false;
+  class CardComponent {
+    constructor(id) {
+      // is this card flipped ?
+      this._flipped = false;
 
-    // has the matching card has been discovered already ?
-    this.matched = false;
+      // has the matching card has been discovered already ?
+      this.matched = false;
 
-    this._id = id;
+      this._id = id;
 
-    this._imageElt = this.getElement().querySelector(".card-wrapper");
-    // TODO Step 3.2: use template literals (backquotes)
-    // TODO Step 7: Update the path for images with 'src/app/components/game/card/assets/card***'
-    this._imageElt.querySelector("img.front-face").src =
-      "./card/assets/card-" + this._id + ".png";
-    this._imageElt.querySelector("img.back-face").src =
-      "./card/assets/back.png";
-  }
-
-  /* method CardComponent.getElement */
-  // TODO Step 7: remove this method
-  CardComponent.prototype.getElement = function getElement() {
-    if (!this._elt) {
-      this._elt = document
-        .getElementById("card-template")
-        .content.cloneNode(true).firstElementChild;
+      this._imageElt = this.getElement().querySelector(".card-wrapper");
+      // TODO Step 3.2: use template literals (backquotes)
+      // TODO Step 7: Update the path for images with 'src/app/components/game/card/assets/card***'
+      this._imageElt.querySelector("img.front-face").src =
+        "./card/assets/card-" + this._id + ".png";
+      this._imageElt.querySelector("img.back-face").src =
+        "./card/assets/back.png";
     }
-    return this._elt;
-  };
 
-  // TODO Step 7 implement getTemplate() {}
 
-  /* method CardComponent.flip */
-  CardComponent.prototype.flip = function flip() {
-    this._imageElt.classList.toggle("flip");
-    this._flipped = !this._flipped;
-  };
+    /* method CardComponent.getElement */
+    // TODO Step 7: remove this method
+    getElement() {
+      if (!this._elt) {
+        this._elt = document
+          .getElementById("card-template")
+          .content.cloneNode(true).firstElementChild;
+      }
+      return this._elt;
+    }
 
-  /* method CardComponent.equals */
-  CardComponent.prototype.equals = function equals(card) {
-    return card._id === this._id;
-  };
+    // TODO Step 7 implement getTemplate() {}
 
-  /* CardComponent.get flipped() */
-  Object.defineProperties(CardComponent.prototype, {
-    flipped: {
-      get: function () {
-        return this._flipped;
-      },
-    },
-  });
+    flip() {
+      this._imageElt.classList.toggle("flip");
+      this._flipped = !this._flipped;
+    }
+
+    equals(card) {
+      return card._id === this._id;
+    }
+
+    /* CardComponent.get flipped() */
+    get flipped() {
+      return this._flipped;
+    }
+  }
 
   // put component in global scope, to be runnable right from the HTML.
   // TODO Step 7 export CardComponent
